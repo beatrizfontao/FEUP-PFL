909c31d13e88dbb14ffae4d7ecfb3ff4d238867a
beatrizfontao <anabfontao@gmail.com>
2023-01-02 11:06:43 +0000

    comments done

--- proj2/utils.pl
+++ proj2/utils.pl
@@ -1,9 +1,17 @@
+/*
+symb(+Piece, -Representation)
+Representation of each piece and empty cell
+*/
 symb(ducko, '\x278a\').
 symb(duckt, '\x278b\').
 symb(swano, '\x2780\').
 symb(swant, '\x2781\').
 symb(e, ' ').
 
+/*
+team(+Piece1, +Piece2)
+Checks if the two pieces belong to the same player
+*/
 team(ducko, swano).
 team(ducko, ducko).
 team(swano, swano).
@@ -12,11 +20,18 @@ team(duckt, duckt).
 team(swant, swant).
 team(e, e).
 
+/*
+winner(+Piece, -Player)
+Given a swan (type of piece), it return the corresponding player(winner)
+*/
 winner(swano, player1).
 winner(swant, player2).
 winner(e, e).
 
-
+/*
+different_team(+Piece1, -Piece2)
+Checks if the two pieces belong to different players
+*/
 different_team(ducko, swant).
 different_team(swant, ducko).
 
@@ -29,26 +44,50 @@ different_team(swano, duckt).
 different_team(swant, swano).
 different_team(swano, swant).
 
+/*
+turn(+CurrentPlayer, -NextPlayer)
+Returns the player that will play the next turn
+*/
 turn(player1, player2).
 turn(player2, player1). 
 
+/*
+playernum(+Player, -Number)
+Returns the number associated with the given player
+*/
 playernum(player1, 1).
 playernum(player2, 2).
 
+/*
+value(+Piece, -Value)
+Given a piece, it returns the corresponding value
+*/
 value(ducko, 10).
 value(swano, 50).
 value(duckt, 10).
 value(swant, 50).
 value(e, 0).
 
+/*
+piece(+Player, +Piece)
+Checks if the given player and piece match
+*/
 piece(player1, ducko).
 piece(player1, swano).
 piece(player2, duckt).
 piece(player2, swant).
 
+/*
+enemy(+Player, -Opponent)
+Return the opponent of the given player
+*/
 enemy(player1, player2).
 enemy(player2, player1).
 
+/*
+row(+Num, -Char)
+Used to print the columns' and rows' numbers and letters
+*/
 row(1, 'A').
 row(1, '1').
 row(2, 'B').
@@ -102,6 +141,10 @@ row(25, '25').
 row(26, 'Z').
 row(26, '26').
 
+/*
+get_piece(+GameState, +Col, +Row, -Piece)
+Given a gamestate and a position, it returns the piece placed there
+*/
 get_piece(GameState, Col, Row, Piece) :-
     nth0(Row, GameState, RowList),
     nth0(Col, RowList, Piece).
